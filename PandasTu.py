ddb288fbb78af293f0de2101c9cbefef232e6f12
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-01-30 09:58:18 +0530

    Basics on pandas library

---
+++ PandasTu.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data = pd.read_csv("nz.csv") #reads the csv file
+# print(data)#prints the csv file
+# print(data.head(10)) #prints only first 10 entries
+
+# print(data.info())
+# #Gives the information of the dataset like number of rows or number of columns etc
+
+# print(data.describe())
+# #gives the various stastistical aspects such as mean, median etc
