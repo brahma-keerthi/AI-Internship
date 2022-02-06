4e8f969c999558e7f107fc3ab15603cecbf0a9a1
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-02-06 14:29:00 +0530

    Bag of words model committed

--- BagOfWords.py
+++ BagOfWords.py
@@ -5,8 +5,7 @@ from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 
-data =  '''The five securities mentioned above are banned for trade under the futures and options segment today because they have exceeded 95% of the market-wide position limit and shall continue remaining in the ban list until their position falls below 80%.
-While on the Futures and Options ban list, no new/fresh F&O positions can be bought or sold for the stock(s), else that trader gets penalised. Traders with existing positions in that security can unwind their positions.'''
+data =  '''Virat is captain. Dhoni is captain. Jadeja is al-rounder'''
 
 ps = PorterStemmer()
 wordnet = WordNetLemmatizer()
@@ -14,7 +13,7 @@ sentence = sent_tokenize(data)
 corpus = []
 
 for i in range(len(sentence)):
-    review = re.sub('[^a-zA-Z]', ' ', sentence[i])
+    review = re.sub('[^a-zA-Z]', ' ', sentence[i])#arranges in alphabetical order
     review = review.lower()#converts to lower alphabets
     review = review.split()#will split words
     review = [ps.stem(word) for word in review if not word in set(stopwords.words("english"))]#removes the stop words
@@ -23,7 +22,7 @@ for i in range(len(sentence)):
 #corpus contains cleaned words
 # until here cleaning of words in done
 # print(data)
-# print(corpus)
+print(corpus)
 
 # Now convertion into matrix starts
 cv = CountVectorizer(max_features = 1500)
